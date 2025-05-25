3150edcd7670db2ad8ff8ef0a672290d1abd460d
AraN <102259330+AraNiff@users.noreply.github.com>
2025-05-25 04:17:59 +0300

    test commit 2

--- obj/Debug/net9.0/123.AssemblyInfo.cs
+++ obj/Debug/net9.0/123.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("123")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601776f6a4976ead0d93ace02cdbf928ecfbde8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8349c21e8fe4fd9fbf86fd3fb2123ea00cbf7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("123")]
 [assembly: System.Reflection.AssemblyTitleAttribute("123")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
