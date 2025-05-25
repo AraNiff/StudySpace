b0de4a48885c1df914abb630d5d25c40b105146f
AraN <102259330+AraNiff@users.noreply.github.com>
2025-05-25 20:36:08 +0300

    Foreach to Study

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
 //fraudulentOrderIDs[3] = "D000";*/
 
-string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+/*string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
@@ -16,4 +16,23 @@ fraudulentOrderIDs[0] = "F000";
 
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process");*/
+
+/*string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}*/
+
+int[] inventory = [200, 450, 700, 175, 250];
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} item in inventory");
--- obj/Debug/net9.0/123.AssemblyInfo.cs
+++ obj/Debug/net9.0/123.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("123")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3150edcd7670db2ad8ff8ef0a672290d1abd460d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7b60c83c2874ca64637642c0991ade34edb14a")]
 [assembly: System.Reflection.AssemblyProductAttribute("123")]
 [assembly: System.Reflection.AssemblyTitleAttribute("123")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
